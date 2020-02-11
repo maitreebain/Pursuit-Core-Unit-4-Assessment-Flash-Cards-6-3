b842f03de10bb6ff54b5d7b6d4f48feb6711e4bf
maitreebain <maitreebain@gmail.com>
2020-02-11 11:36:43 -0500

    loaded up empty cells, work on create view

--- Unit4Assessment/Unit4Assessment/View Controllers/CardsViewController.swift
+++ Unit4Assessment/Unit4Assessment/View Controllers/CardsViewController.swift
@@ -10,11 +10,60 @@ import UIKit
 
 class CardsViewController: UIViewController {
 
+    private let cardsView = CardsView()
     
+    override func loadView() {
+        view = cardsView
+    }
+    
+    var cards = [CardsInfo]() {
+        didSet {
+            cardsView.collectionView.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .red
+        cardsView.collectionView.delegate = self
+        cardsView.collectionView.dataSource = self
+        
+        //register cell here
+        cardsView.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cardsCell")
     }
+}
+
+extension CardsViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let maxSize: CGSize = UIScreen.main.bounds.size
+        let spacingBetweenItems: CGFloat = 10
+        let numberOfItems: CGFloat = 1
+        let itemHeight: CGFloat = maxSize.height * 0.30
+        let totalSpacing: CGFloat = (2 * spacingBetweenItems) + (numberOfItems - 1) * spacingBetweenItems
+        let itemWidth: CGFloat = (maxSize.width - totalSpacing) / numberOfItems
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+      return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    }
+}
+
+extension CardsViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cardsCell", for: indexPath)
+        
+        cell.backgroundColor = .blue
+        
+        return cell
+    }
+    
+    //didselect
+    
 }
