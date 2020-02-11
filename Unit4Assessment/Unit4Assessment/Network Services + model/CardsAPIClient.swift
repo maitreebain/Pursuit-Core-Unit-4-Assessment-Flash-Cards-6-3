c01c99aa4a696f1db7001cb4fada2396ae46f22a
maitreebain <maitreebain@gmail.com>
2020-02-11 10:44:29 -0500

    api client finished, working on tab bar controller

--- Unit4Assessment/Unit4Assessment/Network Services + model/CardsAPIClient.swift
+++ Unit4Assessment/Unit4Assessment/Network Services + model/CardsAPIClient.swift
@@ -11,8 +11,31 @@ import NetworkHelper
 
 struct CardsAPIClient {
     
-    static func getCards(completion: @escaping (Result<[CardsData], AppError>) -> ()) {
+    static func getCards(completion: @escaping (Result<[CardsInfo], AppError>) -> ()) {
+        let endPointURL = "https://5daf8b36f2946f001481d81c.mockapi.io/api/v2/cards"
         
+        guard let url = URL(string: endPointURL) else {
+            completion(.failure(.badURL(endPointURL)))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            
+            switch result {
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do {
+                    let card = try JSONDecoder().decode(CardsData.self, from: data)
+                    
+                    completion(.success(card.cards))
+                } catch {
+                    
+                }
+            }
+        }
     }
 }
 
